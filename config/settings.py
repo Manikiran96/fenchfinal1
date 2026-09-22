73cad03ba4a73f3904e857ce093a325b9ea68c12
Manikiran96 <167034465+Manikiran96@users.noreply.github.com>
2026-09-22 10:39:54 +0530

    Update settings.py
    hardcoded the db credentials

--- config/settings.py
+++ config/settings.py
@@ -59,9 +59,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {"default": {
     
     "ENGINE": "django.db.backends.postgresql",
-    "NAME": os.getenv("POSTGRES_DB", "solar_erp"),
-    "USER": os.getenv("POSTGRES_USER", "solar"),
-    "PASSWORD": os.getenv("POSTGRES_PASSWORD", "solar_pass"),
+    "NAME": 'solae_erp',#os.getenv("POSTGRES_DB", "solar_erp"),
+    "USER": 'solar',#os.getenv("POSTGRES_USER", "solar"),
+    "PASSWORD": 'admin@123',#os.getenv("POSTGRES_PASSWORD", "solar_pass"),
     "HOST": os.getenv("POSTGRES_HOST", "localhost"),
     "PORT": os.getenv("POSTGRES_PORT", "5432"),
 }}
